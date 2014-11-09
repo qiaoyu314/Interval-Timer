325ae428a4703fb323f80b40c3e0643d6228351c
qiaoyu314 <vinkyqy@gmail.com>
2014-11-09 11:08:58 -0600

    polish public timers list layout

--- Interval Timer/YQCollectionViewController.swift
+++ Interval Timer/YQCollectionViewController.swift
@@ -10,58 +10,20 @@ import UIkit
 
 
 class YQCollectionViewController: UICollectionViewController{
-
+    
     var timerList:[timerForDisplay] = []  //store the timer on the cloud
     
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     var timerJosn: Array<AnyObject> = []
-    
+    var fromIndex: Int = 0  //the starting index of the timers for next loading
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
     override func viewDidAppear(animated: Bool) {
-        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
-        //indicator.startAnimating()
-        
-        var URL = "http://0.0.0.0:8000/timers"
-        var request = HTTPTask()
-        request.responseSerializer = JSONResponseSerializer()
-        request.GET(URL, parameters: nil, success: {(response: HTTPResponse) in
-            if response.responseObject != nil{
-                self.timerJosn = response.responseObject as Array<AnyObject>
-                
-                var timer: timerForDisplay
-                var tD: Dictionary<String, AnyObject>
-                for t in self.timerJosn{
-                    tD = t as Dictionary<String, AnyObject>
-                    timer = timerForDisplay()
-                    timer.name = tD["name"] as String
-                    timer.roundLength = tD["roundLength"] as Int
-                    timer.restLength = tD["restLength"] as Int
-                    timer.coolDownLength = tD["cooldownLength"] as Int
-                    timer.warmUpLength = tD["warmUpLength"] as Int
-                    timer.cycle = tD["cycle"] as Int
-                    timer.creationTime = tD["creationTime"] as String
-                    
-                    self.timerList.append(timer)
-                }
-                
-                //UI change needs to be made in UI thread (main thread)
-                dispatch_async(dispatch_get_main_queue(), {
-                    // Your code to run on the main queue/thread
-                    self.indicator.stopAnimating()
-                    self.indicator.removeFromSuperview()
-                    self.collectionView.reloadData()
-                });
-                
-                
-            }
-            }, failure: {(error: NSError, response: HTTPResponse?) in
-                //handle error
-        })
+        loadTimers(true)
     }
     
     //how many cells in a section
@@ -73,10 +35,10 @@ class YQCollectionViewController: UICollectionViewController{
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
         
         var cell: YQCollectionCellView = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as YQCollectionCellView;
-        var timerInfo = "Name: " + timerList[indexPath.row].name + "\n"
-        timerInfo += "Round Timer: " + String(timerList[indexPath.row].roundLength) + "\n"
-        timerInfo += "Rest Time: " + String(timerList[indexPath.row].restLength) + "\n"
-        timerInfo += "Creation Time: " + timerList[indexPath.row].creationTime
+        var timerInfo = timerList[indexPath.row].name
+        //        timerInfo += "Round Timer: " + String(timerList[indexPath.row].roundLength) + "\n"
+        //        timerInfo += "Rest Time: " + String(timerList[indexPath.row].restLength) + "\n"
+        //        timerInfo += "Creation Time: " + timerList[indexPath.row].creationTime
         
         cell.nameCell.text = timerInfo
         cell.downloadButton.indexPath = indexPath
@@ -101,6 +63,67 @@ class YQCollectionViewController: UICollectionViewController{
     @IBAction func toggleSizeMenu(sender: AnyObject) {
         self.toggleSideMenuView()
     }
+    
+    //refresh the timer list
+    @IBAction func refresh(sender: AnyObject) {
+        loadTimers(true)
+    }
+    
+    
+    @IBAction func showDetail(sender: UIButton) {
+        let descripition = "Test"
+        let alertController = UIAlertController(title: "Description", message: descripition, preferredStyle: .Alert)
+        let OKaction = UIAlertAction(title: "OK", style: .Default){(action) in
+        
+        }
+        alertController.addAction(OKaction)
+        
+        self.presentViewController(alertController, animated: true, completion: {})
+    }
+    
+    
+    func loadTimers(fromZero: Bool){
+        if fromZero{
+            self.fromIndex = 0
+        }
+        indicator.startAnimating()
+        
+        var URL = "http://0.0.0.0:8000/timers"
+        var request = HTTPTask()
+        request.responseSerializer = JSONResponseSerializer()
+        request.GET(URL, parameters: nil, success: {(response: HTTPResponse) in
+            if response.responseObject != nil{
+                self.timerJosn = response.responseObject as Array<AnyObject>
+                var timer: timerForDisplay
+                var tD: Dictionary<String, AnyObject>
+                for t in self.timerJosn{
+                    tD = t as Dictionary<String, AnyObject>
+                    timer = timerForDisplay()
+                    timer.name = tD["name"] as String
+                    timer.roundLength = tD["roundLength"] as Int
+                    timer.restLength = tD["restLength"] as Int
+                    timer.coolDownLength = tD["cooldownLength"] as Int
+                    timer.warmUpLength = tD["warmUpLength"] as Int
+                    timer.cycle = tD["cycle"] as Int
+                    timer.creationTime = tD["creationTime"] as String
+                    
+                    self.timerList.append(timer)
+                }
+                
+                //UI change needs to be made in UI thread (main thread)
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.indicator.stopAnimating()
+                    self.indicator.hidden = true
+                    self.collectionView.reloadData()
+                });
+                
+                
+            }
+            }, failure: {(error: NSError, response: HTTPResponse?) in
+                //handle error
+        })
+        
+    }
 }
 
 class timerForDisplay{
@@ -111,6 +134,6 @@ class timerForDisplay{
     var cycle: Int = 0
     var coolDownLength: Int = 0
     var creationTime: String = ""
-
+    
 }
 
